2656ea346643b8352714a21ff9d38c24f4186142
Ivan Geevo <dbowgaming@gmail.com>
2024-06-22 22:26:53 +0300

    1.5 update // bump

--- src/main/java/ivangeevo/mobsalwaysdrop/mixin/MobEntityMixin.java
+++ src/main/java/ivangeevo/mobsalwaysdrop/mixin/MobEntityMixin.java
@@ -18,11 +18,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Arrays;
 
 @Mixin(MobEntity.class)
-public abstract class MobEntityMixin extends LivingEntity {
-    @Shadow private boolean canPickUpLoot;
+public abstract class MobEntityMixin extends LivingEntity
+{
     @Shadow @Final protected float[] armorDropChances;
     @Shadow @Final protected float[] handDropChances;
-
     @Shadow protected abstract float getDropChance(EquipmentSlot slot);
 
     protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
@@ -31,7 +30,8 @@ public abstract class MobEntityMixin extends LivingEntity {
 
     /** Change armor and item drop chances to 1.0F (100% drop chance). **/
     @Inject(method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V", at = @At("TAIL"))
-    private void injectedConstructor(EntityType entityType, World world, CallbackInfo ci) {
+    private void injectedConstructor(EntityType entityType, World world, CallbackInfo ci)
+    {
         Arrays.fill(this.armorDropChances, 1.0F);
         Arrays.fill(this.handDropChances, 1.0F);
     }
@@ -39,15 +39,18 @@ public abstract class MobEntityMixin extends LivingEntity {
     /** Removes the allowDrops boolean check so that it will always drop equipment regardless of cause of death. **/
     // Also added a minimum durability drop int so there isn't so many "empty damage" items.
     @Inject(method = "dropEquipment", at = @At("HEAD"), cancellable = true)
-    private void injectedDropEquipment(DamageSource source, int lootingMultiplier, boolean allowDrops, CallbackInfo ci) {
+    private void injectedDropEquipment(DamageSource source, int lootingMultiplier, boolean allowDrops, CallbackInfo ci)
+    {
         super.dropEquipment(source, lootingMultiplier, allowDrops);
-        for (EquipmentSlot equipmentSlot : EquipmentSlot.values()) {
+        for (EquipmentSlot equipmentSlot : EquipmentSlot.values())
+        {
             boolean bl;
             ItemStack itemStack = this.getEquippedStack(equipmentSlot);
             float f = this.getDropChance(equipmentSlot);
             boolean bl2 = bl = f > 1.0f;
             if (itemStack.isEmpty() || EnchantmentHelper.hasVanishingCurse(itemStack) || !(Math.max(this.random.nextFloat() - (float)lootingMultiplier * 0.01f, 0.0f) < f)) continue;
-            if (!bl && itemStack.isDamageable()) {
+            if (!bl && itemStack.isDamageable())
+            {
                 int minDurabilityDrop = 10;
                 itemStack.setDamage(itemStack.getMaxDamage() - minDurabilityDrop - this.random.nextInt(1 + this.random.nextInt(Math.max(itemStack.getMaxDamage() - 3, 1))));
             }
