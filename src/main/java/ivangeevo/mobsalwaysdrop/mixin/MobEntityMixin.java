cc78f05203bc2dfbb3b83f0f6f7dbf95c0b6d984
Ivan Geevo <dbowgaming@gmail.com>
2024-08-11 16:55:10 +0300

    fix a bug where skeletons weren't dropping their bows (removed causedByPlayer || bl) check

--- src/main/java/ivangeevo/mobsalwaysdrop/mixin/MobEntityMixin.java
+++ src/main/java/ivangeevo/mobsalwaysdrop/mixin/MobEntityMixin.java
@@ -44,7 +44,7 @@ public abstract class MobEntityMixin extends LivingEntity
     @Inject(method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)V", at = @At("TAIL"))
     private void injectedConstructor(EntityType entityType, World world, CallbackInfo ci)
     {
-        if (!MobsAlwaysDropMod.getInstance().settings.isEquipmentDropsEnabled())
+        if ( !MobsAlwaysDropMod.getInstance().settings.isEquipmentDropsEnabled() )
         {
             return;
         }
@@ -53,10 +53,6 @@ public abstract class MobEntityMixin extends LivingEntity
         Arrays.fill(this.handDropChances, 1.0F);
     }
 
-    /**
-     * Removes the allowDrops boolean check so that it will always drop equipment regardless of cause of death.
-     **/
-    // Also added a minimum durability drop int condition, so there isn't so many "empty damage" items.
     //@Inject(method = "dropEquipment(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;Z)V", at = @At("HEAD"), cancellable = true)
     private void OGinjectedDropEquipment(ServerWorld world, DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
         super.dropEquipment(world, source, causedByPlayer);
@@ -73,10 +69,15 @@ public abstract class MobEntityMixin extends LivingEntity
                 this.dropStack(itemStack);
                 this.equipStack(equipmentSlot, ItemStack.EMPTY);
             }
-bu
+
             ci.cancel();
         }
     }
+
+    /**
+     * Removes the (causedByPlayer || bl) boolean checks so that it will always drop equipment regardless of cause of death.
+     **/
+    // Also added a minimum durability drop int condition, so there isn't so many "empty damage" items.
 
     @Inject(method = "dropEquipment(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;Z)V", at = @At("HEAD"), cancellable = true)
     private void injectedDropEquipment(ServerWorld world, DamageSource source, boolean causedByPlayer, CallbackInfo ci) {
@@ -97,7 +98,7 @@ bu
                     }
                 }
 
-                if (!itemStack.isEmpty() && !EnchantmentHelper.hasAnyEnchantmentsWith(itemStack, EnchantmentEffectComponentTypes.PREVENT_EQUIPMENT_DROP) && (causedByPlayer || bl) && this.random.nextFloat() < f) {
+                if (!itemStack.isEmpty() && !EnchantmentHelper.hasAnyEnchantmentsWith(itemStack, EnchantmentEffectComponentTypes.PREVENT_EQUIPMENT_DROP) && this.random.nextFloat() < f) {
                     if (!bl && itemStack.isDamageable()) {
                         int minDurabilityDrop = 10;
                         itemStack.setDamage(itemStack.getMaxDamage() - minDurabilityDrop - this.random.nextInt(1 + this.random.nextInt(Math.max(itemStack.getMaxDamage() - 3, 1))));
